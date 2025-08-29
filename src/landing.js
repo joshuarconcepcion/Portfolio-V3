2d7924bf410879c67c1875e5b59d1f81029c021b
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-08-29 15:06:53 -0700

    Finished left side of landing page. Added links for github and linkedin + styling

--- src/landing.js
+++ src/landing.js
@@ -1,4 +1,6 @@
-import characterImage from './media/images/charImage.png'
+import characterImage from './media/images/charImage.png';
+import p1IconImg from './media/images/p1-icon.png';
+import p2IconImg from './media/images/p2-icon.png';
 
 const container = document.querySelector('main');
 
@@ -17,6 +19,13 @@ const loadLandingLeft = () => {
     landingLeftMediaContainer.classList.add('landing-left-media-container');
     const landingLeftTextContainer = document.createElement('div');
     landingLeftTextContainer.classList.add('landing-left-text-container');
+    const landingLeftP1Container = document.createElement('div');
+    landingLeftP1Container.classList.add('p1-container');
+    const landingLeftP2Container = document.createElement('div');
+    landingLeftP2Container.classList.add('p2-container');
+
+    landingLeftTextContainer.appendChild(landingLeftP1Container);
+    landingLeftTextContainer.appendChild(landingLeftP2Container);
 
     const nameHeading = document.createElement('h1');
     nameHeading.textContent = 'JOSHUA CONCEPCION';
@@ -32,22 +41,53 @@ const loadLandingLeft = () => {
     introText.classList.add('intro-text');
     introText.classList.add('typewriter');
 
-    landingLeftTextContainer.appendChild(introText);
+    const p1Icon = new Image();
+    p1Icon.src = p1IconImg;
+    p1Icon.classList.add('p-icon-img');
+
+    landingLeftP1Container.appendChild(p1Icon);
+    landingLeftP1Container.appendChild(introText);
+
+    const replyTextContainer = document.createElement('div');
+    replyTextContainer.classList.add('reply-text-container');
+    const replyLink1 = document.createElement('a');
+    replyLink1.classList.add('reply-link');
+    replyLink1.textContent = `"LinkedIn"`;
+    replyLink1.href = 'https://www.linkedin.com/in/joshuareyesconcepcion/';
+    replyLink1.target = '_blank';
+    const replyLink2 = document.createElement('a');
+    replyLink2.classList.add('reply-link');
+    replyLink2.textContent = `"GitHub"`;
+    replyLink2.href = 'https://github.com/joshuarconcepcion';
+    replyLink2.target = '_blank';
+
+    replyTextContainer.appendChild(replyLink1);
+    replyTextContainer.appendChild(replyLink2);
+
+    const p2Icon = new Image();
+    p2Icon.src = p2IconImg;
+    p2Icon.classList.add('p-icon-img')
+
+    landingLeftP2Container.appendChild(replyTextContainer);
+    landingLeftP2Container.appendChild(p2Icon);
 
     landingLeft.appendChild(landingLeftMediaContainer);
     landingLeft.appendChild(landingLeftTextContainer);
 
     container.appendChild(landingLeft);
 
-    const text = `'Hello, I'm Joshua, a computer science graduate passionate about cybersecurity and digital forensics
-    with a background in web development.'`;
+    const text = `"Hey, I'm Joshua, a computer science graduate 
+    currently exploring web development, cybersecurity,
+     and digital forensics. What can I do for you?"`;
     let i = 0;
 
     const typeWriter = () => {
         if (i < text.length) {
             introText.textContent += text.charAt(i);
             i++;
-            setTimeout(typeWriter,30);
+            setTimeout(typeWriter,20);
+        } else {
+        landingLeftP2Container.style.display = 'flex';
         }
     }
 
@@ -57,6 +97,11 @@ const loadLandingLeft = () => {
 const loadLandingRight = () => {
     const landingRight = document.createElement('div');
     landingRight.classList.add('landing-right');
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container');
+
+    landingRight.appendChild(projectContainer);
 
     container.appendChild(landingRight);
 }
