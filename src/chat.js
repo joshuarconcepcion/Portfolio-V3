9cdb0955e610cc5cfd2fa323d7ccbec844d17f83
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-08-30 18:48:52 -0700

    Added categories for projects, added additional description + reply option

--- src/chat.js
+++ src/chat.js
@@ -4,16 +4,17 @@ import p2IconImg from './media/images/p2-icon.png';
 export function loadChat(container) {
     const chatContainer = document.createElement('div');
     chatContainer.classList.add('chat-container');
+
+    loadIntroText(chatContainer, () => loadReplyText(chatContainer));
+
+    container.appendChild(chatContainer);
+}
+
+const loadIntroText = (container, onComplete) => {
     const p1Container = document.createElement('div');
     p1Container.classList.add('p1-container');
-    const p2Container = document.createElement('div');
-    p2Container.classList.add('p2-container');
-
-    chatContainer.appendChild(p1Container);
-    chatContainer.appendChild(p2Container);
 
     const introText = document.createElement('h1');
-    introText.classList.add('intro-text');
     introText.classList.add('typewriter');
 
     const p1Icon = new Image();
@@ -23,30 +24,7 @@ export function loadChat(container) {
     p1Container.appendChild(p1Icon);
     p1Container.appendChild(introText);
 
-    const replyTextContainer = document.createElement('div');
-    replyTextContainer.classList.add('reply-text-container');
-    const replyLink1 = document.createElement('a');
-    replyLink1.classList.add('reply-link');
-    replyLink1.textContent = `"LinkedIn"`;
-    replyLink1.href = 'https://www.linkedin.com/in/joshuareyesconcepcion/';
-    replyLink1.target = '_blank';
-    const replyLink2 = document.createElement('a');
-    replyLink2.classList.add('reply-link');
-    replyLink2.textContent = `"GitHub"`;
-    replyLink2.href = 'https://github.com/joshuarconcepcion';
-    replyLink2.target = '_blank';
-
-    replyTextContainer.appendChild(replyLink1);
-    replyTextContainer.appendChild(replyLink2);
-
-    const p2Icon = new Image();
-    p2Icon.src = p2IconImg;
-    p2Icon.classList.add('p-icon-img')
-
-    p2Container.appendChild(replyTextContainer);
-    p2Container.appendChild(p2Icon);
-
-    container.appendChild(chatContainer);
+    container.appendChild(p1Container);
 
     const text = `"Hey, I'm Joshua, a computer science graduate 
     currently exploring web development, cybersecurity,
@@ -59,8 +37,84 @@ export function loadChat(container) {
             i++;
             setTimeout(typeWriter,20);
         } else {
-        p2Container.style.display = 'flex';
+            if (onComplete) onComplete();
         }
+    }
+
+    typeWriter();
+}
+
+const loadReplyText = (container) => {
+    const p2Container = document.createElement('div');
+    p2Container.classList.add('p2-container');
+
+    const replyTextContainer = document.createElement('div');
+    replyTextContainer.classList.add('reply-text-container');
+    
+    const replyLink1 = document.createElement('a');
+    replyLink1.classList.add('reply-link');
+    replyLink1.textContent = `"LinkedIn"`;
+    replyLink1.href = 'https://www.linkedin.com/in/joshuareyesconcepcion/';
+    replyLink1.target = '_blank';
+    
+    const replyLink2 = document.createElement('a');
+    replyLink2.classList.add('reply-link');
+    replyLink2.textContent = `"GitHub"`;
+    replyLink2.href = 'https://github.com/joshuarconcepcion';
+    replyLink2.target = '_blank';
+
+    const replyBtn = document.createElement('button');
+    replyBtn.classList.add('reply-btn');
+    replyBtn.textContent = '"Tell me about yourself"';
+    replyBtn.addEventListener('click', () => {
+        loadLearnMoreReply(container);
+    })
+
+    replyTextContainer.appendChild(replyLink1);
+    replyTextContainer.appendChild(replyLink2);
+    replyTextContainer.appendChild(replyBtn);
+
+    const p2Icon = new Image();
+    p2Icon.src = p2IconImg;
+    p2Icon.classList.add('p-icon-img')
+
+    p2Container.appendChild(replyTextContainer);
+    p2Container.appendChild(p2Icon);
+
+    container.appendChild(p2Container);
+
+    p2Container.style.display = 'flex';
+}
+
+const loadLearnMoreReply = (container) => {
+    const p3Container = document.createElement('div');
+    p3Container.classList.add('p3-container'); 
+
+    const learnMoreText = document.createElement('h1');
+    learnMoreText.classList.add('typewriter');
+
+    const p1Icon = new Image();
+    p1Icon.src = p1IconImg;
+    p1Icon.classList.add('p-icon-img');
+
+    p3Container.appendChild(p1Icon);
+    p3Container.appendChild(learnMoreText);
+
+    container.appendChild(p3Container);
+
+    const text = `"When I'm not coding, you can usually find me 
+    cafe hopping, exploring the city with my cat, 
+    or rewatching old cowboy movies. This space is a growing archive 
+    of the projects I’ve built and the ideas I’m exploring 
+    in design and development."`;
+    let i = 0;
+
+    const typeWriter = () => {
+        if (i < text.length) {
+            learnMoreText.textContent += text.charAt(i);
+            i++;
+            setTimeout(typeWriter,20);
+        } 
     }
 
     typeWriter();
--- src/projects.js
+++ src/projects.js
@@ -5,12 +5,46 @@ export function loadProjects(container) {
     const projectContainer = document.createElement('div');
     projectContainer.classList.add('project-container');
 
+    const header = document.createElement('h1');
+    header.classList.add('project-title');
+    header.textContent = 'PROJECT APPENDIX';
+    projectContainer.appendChild(header);
+
     const projectHeader = document.createElement('h1');
     projectHeader.classList.add('project-header');
-    projectHeader.textContent = 'PROJECTS:';
-
+    projectHeader.textContent = 'FRONTEND:';
     projectContainer.appendChild(projectHeader);
     loadProjectList(projectContainer);
+
+    const projectHeader1 = document.createElement('h1');
+    projectHeader1.classList.add('project-header');
+    projectHeader1.textContent = 'DIGITAL FORENSICS:';
+    projectContainer.appendChild(projectHeader1);
+    loadProjectList1(projectContainer);
+
+    const projectHeader2 = document.createElement('h1');
+    projectHeader2.classList.add('project-header');
+    projectHeader2.textContent = 'EMBEDDED SYSTEMS:';
+    projectContainer.appendChild(projectHeader2);
+    loadProjectList2(projectContainer);
+
+    const projectHeader3 = document.createElement('h1');
+    projectHeader3.classList.add('project-header');
+    projectHeader3.textContent = 'SYSTEMS ADMINISTRATION:';
+    projectContainer.appendChild(projectHeader3);
+    loadProjectList3(projectContainer);
+
+    const projectHeader4 = document.createElement('h1');
+    projectHeader4.classList.add('project-header');
+    projectHeader4.textContent = 'DESIGN/CREATIVE CODING:';
+    projectContainer.appendChild(projectHeader4);
+    loadProjectList4(projectContainer);
+
+    const projectHeader5 = document.createElement('h1');
+    projectHeader5.classList.add('project-header');
+    projectHeader5.textContent = 'SYSTEMS DESIGN:';
+    projectContainer.appendChild(projectHeader5);
+    loadProjectList5(projectContainer);
 
     pageRight.appendChild(projectContainer);
 
@@ -18,28 +52,21 @@ export function loadProjects(container) {
 }
 
 const loadProjectList = (container) => {
-    const project1 = document.createElement('a');
-    project1.classList.add('project-link');
-    project1.textContent = `MOTION DETECTION CAMERA`;
-    project1.href = 'https://twilight-skate-757.notion.site/Motion-Detection-Camera-System-1e9794250ea580759b71e1b4489a8b72';
-    project1.target = '_blank';
+    const project = document.createElement('a');
+    project.classList.add('project-link');
+    project.textContent = `TODO LIST PROTOTYPE`;
+    project.href = 'https://joshuarconcepcion.github.io/ToDo-List/';
+    project.target = '_blank';
 
-    container.appendChild(project1);
+    container.appendChild(project);
+}
 
-    const project2 = document.createElement('a');
-    project2.classList.add('project-link');
-    project2.textContent = `UNIX LAB`;
-    project2.href = 'https://twilight-skate-757.notion.site/UNIX-System-Administration-Project-1e9794250ea58049b4eeef7fc39b2a70';
-    project2.target = '_blank';
-
-    container.appendChild(project2);
-
+const loadProjectList1 = (container) => {
     const project3 = document.createElement('a');
     project3.classList.add('project-link');
     project3.textContent = `WINDOWS FORENSIC INVESTIGATION & DATA SANITIZATION`;
     project3.href = 'https://twilight-skate-757.notion.site/Case-Report-Windows-11-Forensic-Acquisition-Recovery-and-Data-Sanitization-253794250ea580889413f34c6d77f4b9';
     project3.target = '_blank';
-
     container.appendChild(project3);
 
     const project4 = document.createElement('a');
@@ -47,7 +74,6 @@ const loadProjectList = (container) => {
     project4.textContent = `WINDOWS FORENSIC TRIAGE & RAM CAPTURE`;
     project4.href = 'https://twilight-skate-757.notion.site/Case-Report-Triage-Evidence-Collection-and-Analysis-257794250ea58047ac83c2cb2aa2e16e';
     project4.target = '_blank';
-
     container.appendChild(project4);
 
     const project5 = document.createElement('a');
@@ -55,22 +81,41 @@ const loadProjectList = (container) => {
     project5.textContent = `MOBILE DEVICE FORENSIC ACQUISITION & ARTIFACT ANALYSIS`;
     project5.href = 'https://twilight-skate-757.notion.site/iPhone-Forensic-Acquisition-Analysis-Report-25d794250ea580189727c63a25001493';
     project5.target = '_blank';
-
     container.appendChild(project5);
+}
 
+const loadProjectList2 = (container) => {
+    const project1 = document.createElement('a');
+    project1.classList.add('project-link');
+    project1.textContent = `MOTION DETECTION CAMERA`;
+    project1.href = 'https://twilight-skate-757.notion.site/Motion-Detection-Camera-System-1e9794250ea580759b71e1b4489a8b72';
+    project1.target = '_blank';
+    container.appendChild(project1);
+}
+
+const loadProjectList3 = (container) => {
+    const project2 = document.createElement('a');
+    project2.classList.add('project-link');
+    project2.textContent = `UNIX ADMINISTRATION LAB`;
+    project2.href = 'https://twilight-skate-757.notion.site/UNIX-System-Administration-Project-1e9794250ea58049b4eeef7fc39b2a70';
+    project2.target = '_blank';
+    container.appendChild(project2);
+}
+
+const loadProjectList4 = (container) => {
     const project6 = document.createElement('a');
     project6.classList.add('project-link');
     project6.textContent = `UNITY 3D GAME`;
     project6.href = 'https://github.com/mosawar/FPS-Zombies/tree/Final-Main';
     project6.target = '_blank';
-
     container.appendChild(project6);
+}
 
+const loadProjectList5 = (container) => {
     const project7 = document.createElement('a');
     project7.classList.add('project-link');
     project7.textContent = `INTELLIDRIVE MOCKUP`;
     project7.href = 'https://www.joshuarconcepcion.com/media/[CS250]%20Final%20Project%20Report%20-%20IntelliDrive.pdf';
     project7.target = '_blank';
-
     container.appendChild(project7);
 }
